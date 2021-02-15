d5880bcef77544bc87724578fd28825e35162a3b
Bianca Nunes <bianca.nunes@redfox.tech>
2021-02-15 13:39:59 -0300

    create node app

---
+++ src/index.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+const cors = require("cors");
+
+const config = require("./config");
+
+var app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+app.listen(config.PORT, () => {
+  console.log(`Servidor rodando na porta ${config.PORT}`);
+});
+
+module.exports = app;
